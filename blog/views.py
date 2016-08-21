034f92cc0742586e98f37775c42713c3765e8a09
사명구(Sa Myeonggu) <quest4i@me.com>
2016-08-21 23:04:35 +0900

    버그 수정, homework1: draft, publish 기능 구현

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.created_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
 
@@ -38,9 +38,21 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.created_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
 
     form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_list', pk=pk)
+
